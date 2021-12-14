8e75f8d8e21251062058b977eb0cd253c4f6fe35
Nick <kolan.eremeev1001@gmail.com>
2021-12-15 00:31:50 +0300

    Enemy Health Bar

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _health = 100;
+    [SerializeField] private int _maxHealth = 100;
+    [SerializeField] private int _health;
+
+    [SerializeField] private Image _healthBar;
+    [SerializeField] private float _timeToChangeHealth = 1f;
+
     private Coroutine _coroutine;
+    private Coroutine _healthCoroutine;
+
+    private float _lerpValue;
+
+    private void Start()
+    {
+        _health = _maxHealth;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -32,6 +45,29 @@ public class Enemy : MonoBehaviour
 
     public void TakeDamage(int value)
     {
+        if (_healthCoroutine == null)
+        {
+            _healthCoroutine = StartCoroutine(ChangeHealth(_health));
+        }
+        else
+        {
+            StopCoroutine(_healthCoroutine);
+            _healthCoroutine = StartCoroutine(ChangeHealth(_healthBar.fillAmount * _maxHealth));
+        }
+
         _health -= value;
+    }
+
+    private IEnumerator ChangeHealth(float oldValue)
+    {
+        float timer = 0f;
+
+        while (timer < _timeToChangeHealth)
+        {
+            _healthBar.fillAmount = Mathf.Lerp(oldValue / _maxHealth, _health / (float)_maxHealth, timer / _timeToChangeHealth);
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
     }
 }
